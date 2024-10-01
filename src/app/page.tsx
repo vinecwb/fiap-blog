a49c0ff1151f3f25f05b247bf30528011314c8d1
Claudio Matheus do Rosario <claudio.rosario@logcomex.com>
2024-10-01 17:57:26 -0300

    Changed the UI of the entire system and some minors fix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react"
-import { Bounce, ToastContainer, toast } from "react-toastify";
+import {useRouter} from "next/navigation";
+import {useState} from "react"
+import {Bounce, toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
+import "./page.css"
 
 export default function Home() {
   const router = useRouter()
@@ -15,11 +16,9 @@ export default function Home() {
 
     try {
       const res = await fetch('http://localhost:3001/auth/login', {
-        method: 'POST',
-        headers: {
+        method: 'POST', headers: {
           'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
+        }, body: JSON.stringify({email, password}),
       });
 
       const data = await res.json();
@@ -47,46 +46,44 @@ export default function Home() {
     }
   };
 
-  return (
-    <main className="flex flex-1 flex-col h-screen w-screen justify-center items-center bg-background-900">
-      <p className="font-sans text-2xl text-fontColor-900">Fiap blog</p>
-      <div className="flex flex-1 flex-col max-h-[45vh] w-[80vw] justify-center items-center bg-background-800 rounded-[10px]">
-        <p className="font-sans text-lg mb-2 text-fontColor-900 bg-background-800">Digite seu email:</p>
-        <input className="font-sans text-lg text-fontColor-900 bg-background-900 border border-fontColor-900 rounded-3xl p-2 w-5/6"
-          type="email"
-          placeholder=""
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <p className="font-sans text-lg mt-2 mb-2 text-fontColor-900 bg-background-800">Digite sua senha:</p>
-        <input className="font-sans text-lg text-fontColor-900 bg-background-900 border border-fontColor-900 rounded-3xl p-2 w-5/6"
-          type="password"
-          placeholder=""
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <div className="flex flex-1 flex-row mt-2 w-[40%] max-h-12 flex-nowrap ml-auto mr-auto justify-between  items-center">
-          <Link
-            className="mt-2 text-center p-2 font-sans bg-fontColor-900 flex-wrap rounded-xl hover:opacity-80 text-[#fff]"
-            href="register">
-            Crie seu cadastro</Link>
-          <button disabled={(password == '' || email == '')} className="w-32 mt-2 p-2 ml-1 font-sans bg-fontColor-900 rounded-xl hover:opacity-80 text-[#fff]"
-            onClick={handleLogin}
-          >Entrar</button>
-        </div>
-      </div>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-        transition={Bounce}
+  return (<main className="flex flex-1 flex-col h-screen w-screen justify-center items-center bg-background-900">
+    <p className="font-sans text-2xl text-fontColor-900 no-select">FIAP BLOG</p>
+    <div className="container">
+      <h2 className="title no-select">
+        Login
+      </h2>
+      <input
+        className="email-field"
+        type="text"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
       />
-    </main>
-  );
+      <input
+        className="password-field"
+        type="password"
+        placeholder="Senha"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="login-button" disabled={(password == '' || email == '')} onClick={handleLogin}>
+        Entrar
+      </button>
+      <a href="register" className="no-select register-button">
+        Cadastrar
+      </a>
+    </div>
+
+    <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+      transition={Bounce}
+    />
+  </main>);
 }
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,143 +1,154 @@
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import {useRouter} from "next/router";
+import {useEffect, useRef, useState} from "react";
+import "./posts.css"
 import Link from "next/link";
+import {isTeacher} from "@/utils/auth";
 
 interface IPost {
-    id: number; 
-    title: string;
-    content: string;
-    authorId: number;
+  id: number;
+  title: string;
+  content: string;
+  authorId: number;
 }
 
 export async function getServerSideProps(context) {
-    const { params, query } = context;
-    const recipientId = query.recipientId;
+  const {params, query} = context;
+  const recipientId = query.recipientId;
 
-    if (!recipientId) {
-        console.error('recipientId is not defined');
-        return { props: { userSender: null, senderId: null } };
+  if (!recipientId) {
+    console.error('recipientId is not defined');
+    return {props: {userSender: null, senderId: null}};
+  }
+
+  try {
+    const response = await fetch(`http://localhost:3001/users/${recipientId}`, {
+      method: 'GET', headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 200) {
+      const userSender = await response.json();
+      return {props: {userSender, senderId: query.senderId}};
+    } else {
+      return {props: {userSender: null, senderId: null}};
     }
-
-    try {
-        const response = await fetch(`http://localhost:3001/users/${recipientId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.status === 200) {
-            const userSender = await response.json();
-            return { props: { userSender, senderId: query.senderId } };
-        } else {
-            return { props: { userSender: null, senderId: null } };
-        }
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-        return { props: { userSender: null, senderId: null } };
-    }
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+    return {props: {userSender: null, senderId: null}};
+  }
 }
 
-export default function PostPage({ userSender, senderId }) {
-    const router = useRouter();
-    const { recipientId } = router.query;
-    const [userName, setUserName] = useState('');
-    const [posts, setPosts] = useState<IPost[]>([]);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [authors, setAuthors] = useState<{ [key: number]: string }>({});
-    const postContainer = useRef<HTMLDivElement>(null);
+export default function PostPage({userSender, senderId}) {
+  const router = useRouter();
+  const {recipientId} = router.query;
+  const [userName, setUserName] = useState('');
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [authors, setAuthors] = useState<{ [key: number]: string }>({});
+  const postContainer = useRef<HTMLDivElement>(null);
+  const [isTeacherUser, setIsTeacherUser] = useState(false);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/posts');
-                if (!response.ok) throw new Error('Erro ao buscar posts');
-                const data = await response.json();
-                setPosts(data);
-                
-                const authorIds = [...new Set(data.map(post => post.authorId))]; 
-                const authorPromises = authorIds.map(id => 
-                    fetch(`http://localhost:3001/users/${id}`).then(res => res.json())
-                );
-                const authorData = await Promise.all(authorPromises);
-                const authorMap = authorData.reduce((acc, author) => {
-                    acc[author.id] = author.email; 
-                    return acc;
-                }, {});
-                setAuthors(authorMap);
-            } catch (error) {
-                console.error('Erro ao buscar posts:', error);
-            }
-        };
+  useEffect(() => {
 
-        fetchPosts();
-    }, []);
-
-    useEffect(() => {
-        if (userSender) {
-            setUserName(userSender.name);
-        }
-    }, [userSender]);
-
-    const handleSearch = async (event) => {
-        event.preventDefault();
-        if (!searchQuery) return;
-
-        try {
-            const response = await fetch(`http://localhost:3001/posts/search?query=${searchQuery}`);
-            if (!response.ok) throw new Error('Erro ao buscar posts');
-            const data = await response.json();
-            setPosts(data);
-        } catch (error) {
-            console.error('Erro ao buscar posts:', error);
-        }
+    const checkIfTeacher = async () => {
+      const result = await isTeacher();
+      setIsTeacherUser(result);
     };
 
-    useEffect(() => {
-        postContainer.current?.scrollTo(0, postContainer.current.scrollHeight);
-    }, [posts]);
+    checkIfTeacher();
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/posts');
+        if (!response.ok) throw new Error('Erro ao buscar posts');
+        const data = await response.json();
+        setPosts(data);
 
-    return (
-        <div className="flex flex-col items-center p-4 bg-background-900 min-h-screen">
-            <h1 className="text-3xl font-bold mb-4 text-fontColor-900">Tela de Posts</h1>
+        const authorIds = [...new Set(data.map(post => post.authorId))];
+        const authorPromises = authorIds.map(id => fetch(`http://localhost:3001/users/${id}`).then(res => res.json()));
+        const authorData = await Promise.all(authorPromises);
+        const authorMap = authorData.reduce((acc, author) => {
+          acc[author.id] = author.email;
+          return acc;
+        }, {});
+        setAuthors(authorMap);
+      } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+      }
+    };
 
-            <div className="flex justify-end w-full mb-4">
-                <Link href="/create">
-                    <button className="bg-fontColor-900 text-white rounded py-2 px-4 hover:opacity-80 mr-2">
-                        Novo Post
-                    </button>
-                </Link>
+    fetchPosts();
+  }, []);
 
-                <Link href="/admin">
-                    <button className="bg-fontColor-900 text-white rounded py-2 px-4 hover:opacity-80">
-                        Administrador
-                    </button>
-                </Link>
-            </div>
+  useEffect(() => {
+    if (userSender) {
+      setUserName(userSender.name);
+    }
+  }, [userSender]);
 
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    if (!searchQuery) return;
 
-            <form onSubmit={handleSearch} className="mb-4">
-                <input
-                    type="text"
-                    placeholder="Buscar posts..."
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className="border border-fontColor-900 p-2 rounded mb-4 bg-background-900 text-fontColor-900"
-                />
-                <button type="submit" className="m-4 bg-fontColor-900 text-white rounded py-2 px-4 hover:opacity-80">Buscar</button>
-            </form>
+    try {
+      const response = await fetch(`http://localhost:3001/posts/search?query=${searchQuery}`);
+      if (!response.ok) throw new Error('Erro ao buscar posts');
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error('Erro ao buscar posts:', error);
+    }
+  };
 
-            <div ref={postContainer} className="w-full max-w-lg">
-                {posts.map((post) => (
-                    <div key={post.id} className="bg-background-800 border border-fontColor-900 p-4 mb-4 rounded shadow">
-                        <Link href={`/posts/${post.id}`}>
-                            <h3 className="text-xl font-semibold text-fontColor-900">{post.title}</h3>
-                        </Link>
-                        <p className="text-gray-700">{post.content}</p>
-                        <p className="text-sm text-gray-500">Autor: {authors[post.authorId] || 'Desconhecido'}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const handleCreate = () => {
+    router.push("/create");
+  }
+
+  const handleAdmin = () => {
+    router.push("/admin");
+  }
+
+  const handleLogOut = () => {
+    router.push("/");
+  }
+
+  useEffect(() => {
+    postContainer.current?.scrollTo(0, postContainer.current.scrollHeight);
+  }, [posts]);
+
+  return (<div className="flex flex-col items-center p-4 bg-background-900 min-h-screen">
+    <h1 className="text-3xl font-bold mb-4 text-fontColor-900">Tela de Posts</h1>
+
+    {isTeacherUser && (
+      <div className="header">
+        <button id="new-post" onClick={handleCreate}>Novo Post</button>
+        <button id="admin" onClick={handleAdmin}>Administrador</button>
+      </div>
+    )}
+
+    <form className="search-bar" onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Buscar posts..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button type="submit">
+        Buscar
+      </button>
+    </form>
+
+    <div ref={postContainer} className="post">
+      {posts.map((post) => (<div key={post.id} className="post-card p-4 mb-4">
+        <Link href={`/posts/${post.id}`}>
+          <h3 className="text-xl font-semibold text-fontColor-900">{post.title}</h3>
+          <p className="text-gray-700">{post.content}</p>
+          <p className="text-sm text-gray-500">Autor: {authors[post.authorId] || 'Desconhecido'}</p>
+        </Link>
+      </div>))}
+    </div>
+    <div>
+      <button onClick={handleLogOut}>Sair</button>
+    </div>
+  </div>);
 }
